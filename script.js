28ea0a4b3ac7267aca0f31bf284f9376694740a0
AHmyrianska <a.hmyrianska@gmail.com>
2022-10-03 16:05:00 +0300

    Added copy function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let passLength = 15;
 let pass1El = document.getElementById("psw-1");
 let pass2El = document.getElementById("psw-2");
 let button = document.getElementById("btn");
+// let clickEl = document.getElementById("click");
 
 
 
@@ -15,7 +16,8 @@ function generatePassword1() {
         let randomIndex = Math.floor(Math.random() * characters.length);
         pass += characters[randomIndex];
     }
-    pass1El.textContent = pass;   
+    pass1El.textContent = pass;
+    // clickEl.textContent = "Click to copy";   
 }
 
 function generatePassword2() {
@@ -33,4 +35,30 @@ button.addEventListener("click", generatePassword2);
 
 
 
+pass1El.onclick = () => {
+  document.execCommand("copy");
+}
 
+pass1El.addEventListener("copy", (event) => {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", pass1El.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
+
+pass2El.onclick = () => {
+    document.execCommand("copy");
+  }
+  
+  pass2El.addEventListener("copy", (event) => {
+    event.preventDefault();
+    if (event.clipboardData) {
+      event.clipboardData.setData("text/plain", pass2El.textContent);
+      console.log(event.clipboardData.getData("text"))
+    }
+  });
+//   'copy' explanation: https://thewebdev.info/2021/09/04/how-to-copy-a-text-string-on-click-with-javascript/
+
+
+            
